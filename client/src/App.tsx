0004a9d0c9be4190a1ceaeca908a2006cd11c2dd
Murilo Pereira <34790144+murilopereirame@users.noreply.github.com>
2024-05-02 17:10:14 +0200

    Added polling to web client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,91 @@
 import React, {useEffect, useState} from "react"
 import {callConnect, callDisconnect, callGetStatus, VpnStatus} from "./request";
-import { ToastContainer } from "react-toastify";
+import {ToastContainer} from "react-toastify";
 
 const App = () => {
-  const [isConnected, setIsConnected] = useState<boolean>(false)
-  const [status, setStatus] = useState<VpnStatus | undefined>()
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [isConnected, setIsConnected] = useState<boolean>(false)
+    const [status, setStatus] = useState<VpnStatus | undefined>()
+    const [isLoading, setIsLoading] = useState<boolean>(true)
 
-  useEffect(() => {
-    callGetStatus().then((status) => {
-      if(status) {
-        setStatus(status)
-        setIsConnected(true)
-      }
-    }).finally(() => setIsLoading(false))
-  }, [])
+    useEffect(() => {
+        callGetStatus().then((status) => {
+            if (status) {
+                setStatus(status)
+                setIsConnected(true)
+            }
+        }).finally(() => setIsLoading(false))
+    }, [])
 
-  const generateStatusMessage = () => {
-    return `Client pKey: ${status?.client}\n` +
-           `Endpoint: ${status?.endpoint}\n` +
-           `Routed IPs: ${status?.allowedIps}\n` +
-           `Uptime: ${status?.uptime}\n`+
-           `Received: ${status?.traffic.received.value.toFixed(2)} ${status?.traffic.received.unit}\n`+
-           `Sent: ${status?.traffic.sent.value.toFixed(2)} ${status?.traffic.sent.unit}`
+    useEffect(() => {
+        let poller;
+        if (isConnected) {
+            poller !== undefined && clearInterval(poller)
+            poller = setInterval(() => {
+                callGetStatus().then((status) => {
+                    if (status) {
+                        setStatus(status)
+                        setIsConnected(true)
+                    }
+                })
+            }, 5000)
+        } else {
+            poller !== undefined && clearInterval(poller)
+        }
+    }, [isConnected]);
 
-  }
+    const generateStatusMessage = () => {
+        return `Client pKey: ${status?.client}\n` +
+            `Endpoint: ${status?.endpoint}\n` +
+            `Routed IPs: ${status?.allowedIps}\n` +
+            `Handshake: ${status?.uptime}\n` +
+            `Received: ${status?.traffic.received.value.toFixed(2)} ${status?.traffic.received.unit}\n` +
+            `Sent: ${status?.traffic.sent.value.toFixed(2)} ${status?.traffic.sent.unit}`
 
-  const switchConnection = async () => {
-    setIsLoading(true)
-    if (isConnected) {
-      setIsConnected(!(await callDisconnect()))
-    } else {
-      setIsConnected(await callConnect())
     }
 
-    callGetStatus().then((status) => {
-      if(status) {
-        setStatus(status)
-        setIsConnected(true)
-      }
-    }).finally(() => setIsLoading(false))
-  }
+    const switchConnection = async () => {
+        setIsLoading(true)
+        if (isConnected) {
+            setIsConnected(!(await callDisconnect()))
+        } else {
+            setIsConnected(await callConnect())
+        }
 
-  return <>
-    {isLoading && <div className="loading-container w-full h-full flex items-center justify-center">
-      <div className="lds-ring">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>}
-    <ToastContainer/>
-    <div className={"flex flex-col items-center h-full justify-center bg-arapawa-900"}>
-      <img className={"w-52"} alt={"Surfwire"} src={"assets/logo.png"}/>
-      <div className={"w-3/4 flex flex-col items-center mt-5"}>
+        callGetStatus().then((status) => {
+            if (status) {
+                setStatus(status)
+                setIsConnected(true)
+            }
+        }).finally(() => setIsLoading(false))
+    }
+
+    return <>
+        {isLoading && <div className="loading-container w-full h-full flex items-center justify-center">
+			<div className="lds-ring">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>}
+        <ToastContainer/>
+        <div className={"flex flex-col items-center h-full justify-center bg-arapawa-900"}>
+            <img className={"w-52"} alt={"Surfwire"} src={"assets/logo.png"}/>
+            <div className={"w-3/4 flex flex-col items-center mt-5"}>
                 <textarea readOnly={true}
                           value={!isConnected ? 'Disconnected' : generateStatusMessage()}
                           style={{minHeight: 190}}
                           className={"text-white max-w-xs w-full bg-transparent border-white resize-none border-2 rounded-xl p-2 cursor-text"}>
                 </textarea>
-        <button
-            className={`mt-2 max-w-xs font-black w-full ${isConnected ? 'bg-red-700' : 'bg-green-600'} text-xl rounded-xl text-white`}
-            onClick={() => switchConnection()}
-        >
-          {isConnected ? 'DISCONNECT' : 'CONNECT'}
-        </button>
-      </div>
-    </div>
-  </>
+                <button
+                    className={`mt-2 max-w-xs font-black w-full ${isConnected ? 'bg-red-700' : 'bg-green-600'} text-xl rounded-xl text-white`}
+                    onClick={() => switchConnection()}
+                >
+                    {isConnected ? 'DISCONNECT' : 'CONNECT'}
+                </button>
+            </div>
+        </div>
+    </>
 }
 
 export default App
